9620f41b1e7917aa38c46ff32e277460bc4971ae
team621 <team621@naver.com>
2022-05-21 10:53:39 +0900

    20220521 - appconfig 리팩터링, 新 할인정책 적용

--- core/src/main/java/hello/core/order/AppConfig.java
+++ core/src/main/java/hello/core/order/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core.order;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPoilcy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemberServie;
 import hello.core.member.MemoryMemberRepository;
@@ -8,10 +10,19 @@ import hello.core.member.MemoryMemberRepository;
 public class AppConfig {
 
     public MemberServie memberServie(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPoilcy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    private MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    private DiscountPolicy discountPolicy() {
+        //return new FixDiscountPoilcy();
+        return new RateDiscountPolicy();
     }
 }
